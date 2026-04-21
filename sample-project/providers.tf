960d56d558decd30967e393c3a29bda96a152a8c
Andrii Sukov <andrii.sukov@undp.org>
2026-04-21 11:58:29 +0000

    Updated backend bucket

--- sample-project/providers.tf
+++ sample-project/providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = var.state_bucket
+    bucket = "tf_state_bucket_as88" 
     prefix = "terraform/fast_state"
     # Impersonate tf service account to access the state bucket
     impersonate_service_account = "asukov-tf-rw@boxwood-bee-484209-r9.iam.gserviceaccount.com"
--- sample-project/variables.tf
+++ sample-project/variables.tf
@@ -3,11 +3,6 @@ variable "project_id" {
   type        = string
 }
 
-variable "state_bucket" {
-  description = "The TF state bucket"
-  type        = string
-}
-
 variable "terraform_service_account" {
   description = "The service account to impersonate for Terraform operations"
   type        = string
